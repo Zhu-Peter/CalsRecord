56bc27f21f216624b1fa20ab1bc5386eac869ee7
Zhu-Peter <gzhu@ualberta.ca>
2024-08-14 15:17:14 -0600

    removed navigation drawer, added about page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,15 @@
 <template>
+
     <nav>
-        <v-toolbar flat app>
-            <v-app-bar-nav-icon class="text-grey" @click="toolbar_drawer = !toolbar_drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title class="font-weight-light text-primary">
-                <span class="text-uppercase text-secondary">Cals</span>
-                <span>Record</span>
-            </v-toolbar-title>
-            <v-btn flat color="grey">
-                <span>Sign In</span>
-                <v-icon right icon="mdi-exit-to-app" class="mx-1"></v-icon>
-            </v-btn>
-        </v-toolbar>
-        <v-app>
-            <v-navigation-drawer app v-model="toolbar_drawer" >
-                <v-app-bar-nav-icon size="large" class="text-primary mt-4 ml-4"
-                    @click="toolbar_drawer = !toolbar_drawer"></v-app-bar-nav-icon>
-                <v-list class="ml-5">
+        <v-toolbar app>
+            
+            <v-menu>
+                <template v-slot:activator="{ props }">
+                    <v-btn color="primary" dark v-bind="props">
+                        <v-app-bar-nav-icon class="text-grey pb-2" @click="toolbar_drawer = !toolbar_drawer"></v-app-bar-nav-icon>
+                    </v-btn>
+                </template>
+                <v-list class="py-4">
                     <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                         <template v-slot:prepend>
                             <v-icon :icon="link.icon"></v-icon>
@@ -23,9 +17,29 @@
                         <v-list-item-title>{{ link.text }}</v-list-item-title>
                     </v-list-item>
                 </v-list>
-            </v-navigation-drawer>
-        </v-app>
+            </v-menu>
+            <v-app-bar-title class="font-weight-light text-primary">
+                <span class="text-uppercase text-secondary">Cals</span>
+                <span>Record</span>
+            </v-app-bar-title>
+            <v-btn flat color="grey">
+                <span>Sign In</span>
+                <v-icon right icon="mdi-exit-to-app" class="mx-1"></v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <!-- <v-navigation-drawer app v-model="toolbar_drawer">
+                <v-list class="ml-2 mt-4">
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <template v-slot:prepend>
+                            <v-icon :icon="link.icon"></v-icon>
+                        </template>
+                        <v-list-item-title>{{ link.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer> -->
     </nav>
+
 </template>
 
 <script>
@@ -35,7 +49,8 @@ export default {
             toolbar_drawer: false,
             links: [
                 { text: 'Dashboard', route: '/', icon: 'mdi-view-dashboard' },
-                { text: 'Settings', route: '/About', icon: 'mdi-account-box' },
+                { text: 'About', route: '/About', icon: 'mdi-information' },
+                { text: 'Settings', route: '/', icon: 'mdi-account-box' }
             ]
         }
     },
