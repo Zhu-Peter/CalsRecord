873e6220364d10c9c0f2b27753e401989641c523
Zhu-Peter <gzhu@ualberta.ca>
2024-08-14 15:27:37 -0600

    minor formatting

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,11 @@
 
     <nav>
         <v-toolbar app>
-            
             <v-menu>
                 <template v-slot:activator="{ props }">
                     <v-btn color="primary" dark v-bind="props">
-                        <v-app-bar-nav-icon class="text-grey pb-2" @click="toolbar_drawer = !toolbar_drawer"></v-app-bar-nav-icon>
+                        <v-app-bar-nav-icon class="text-grey pb-2"
+                            @click="toolbar_drawer = !toolbar_drawer"></v-app-bar-nav-icon>
                     </v-btn>
                 </template>
                 <v-list class="py-4">
@@ -19,8 +19,10 @@
                 </v-list>
             </v-menu>
             <v-app-bar-title class="font-weight-light text-primary">
-                <span class="text-uppercase text-secondary">Cals</span>
-                <span>Record</span>
+                <router-link to="/" style="text-decoration: none;" class="text-primary">
+                    <span class="text-uppercase text-secondary">Cals</span>
+                    <span>Record</span>
+                </router-link>
             </v-app-bar-title>
             <v-btn flat color="grey">
                 <span>Sign In</span>
