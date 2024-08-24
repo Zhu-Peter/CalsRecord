1050107a49b2049606bd9d7a4c003af1e30d2105
Zhu-Peter <gzhu@ualberta.ca>
2024-08-23 18:54:38 -0600

    add app content

--- src/components/CalRecordApp.vue
+++ src/components/CalRecordApp.vue
@@ -1,15 +1,52 @@
 <template>
     <div>
-        <h1>app</h1>
+        <v-container fluid="true">
+            <v-row>
+                <v-col>
+                    <v-card width="100%" height="90%" class="elevation-1 my-8 py-8" justify-center>
+                        <v-card-title-text>
+                            <span class="headline">Enter a food</span>
+                        </v-card-title-text>
+                        <!-- search bar -->
+                        <v-text-field label="Search" v-model="search" prepend-icon="mdi-magnify"></v-text-field>
+
+                    </v-card>
+
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-row class="breakfast">
+                        <v-btn color="primary" @click="addBreakfast">Add Breakfast</v-btn>
+                    </v-row>
+                    <v-row class="lunch">
+                        <v-btn color="primary" @click="addLunch">Add Lunch</v-btn>
+                    </v-row>
+                    <v-row class="dinner">
+                        <v-btn color="primary" @click="addDinner">Add Dinner</v-btn>
+                    </v-row>
+                    <v-row class="snacks">
+
+                        <v-btn color="primary" @click="addSnacks">Add Snacks</v-btn>
+                    </v-row>
+                    <v-row class="snacks">
+
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    name: "CalRecordApp",
+    data() {
+        return {
+            search: ""
+        }
     }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
