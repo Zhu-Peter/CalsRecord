eae333ce753e79b02dad3248ebba94a16e19dee5
Zhu-Peter <gzhu@ualberta.ca>
2024-08-23 19:15:48 -0600

    added link to signup

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,7 @@
         <v-container class="background_with_image bg-grey-darken-4" fluid fill-height>
             <v-row justify="center">
                 <v-card width="70%" height="90%" class="elevation-1 my-8 py-8" justify-center>
-                    <v-card-title >
+                    <v-card-title>
                         <v-card-title-text>
                             <span class="headline">Login</span>
                         </v-card-title-text>
@@ -12,39 +12,33 @@
                         <v-container>
                             <v-row>
                                 <v-col cols="12">
-                                    <v-text-field
-                                        label="Email"
-                                        prepend-icon="mdi-email"
-                                        v-model="email"
-                                        required
-                                    ></v-text-field>
+                                    <v-text-field label="Email" prepend-icon="mdi-email" v-model="email"
+                                        required></v-text-field>
                                 </v-col>
                             </v-row>
                             <v-row>
-                                 <v-col cols="12">
-                                     <v-text-field
-                                        label="Password"
-                                        prepend-icon="mdi-lock"
-                                        v-model="password"
-                                        type="password"
-                                        required
-                                     ></v-text-field>
-                                 </v-col>
-                             </v-row>
-                             <v-row justify="end"> 
-                                 <v-col cols="6" sm="3" md="2">
-                                     <v-btn class="login_btn" color="primary" @click="login">Login</v-btn>
-                                 </v-col>
-                             </v-row>
-                             <v-row>
-                                <v-col>
-                                    <!-- error box -->
-                                     <v-alert v-if="error" color="error">{{ error }}</v-alert>
-                                 <v-col></v-col>
+                                <v-col cols="12">
+                                    <v-text-field label="Password" prepend-icon="mdi-lock" v-model="password"
+                                        type="password" required></v-text-field>
                                 </v-col>
-                             </v-row>
-                         </v-container>
-                     </v-card-text>
+                            </v-row>
+                            <v-row justify="end">
+                                <v-col cols="6" sm="3" md="2">
+                                    <v-btn class="login_btn" color="primary" @click="login">Login</v-btn>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col>
+                                    <v-alert v-if="error" color="error">{{ error }}</v-alert>
+                                </v-col>
+                            </v-row>
+                            <v-row justify="center">
+                                <v-col class="text-center text-body-1">
+                                    <span>Don't have an account? <router-link to="/signup">Register</router-link></span>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card-text>
                 </v-card>
             </v-row>
         </v-container>
@@ -63,7 +57,7 @@ export default {
     },
     methods: {
         login: function () {
-            
+
         },
     }
 
@@ -71,7 +65,7 @@ export default {
 </script>
 
 <style scoped>
-.background_with_image{
+.background_with_image {
     background-image: url('https://images.pexels.com/photos/1247676/pexels-photo-1247676.jpeg');
     background-size: cover;
     background-position: center;
@@ -79,13 +73,15 @@ export default {
     width: 100vw;
     display: flex;
     /* filter: blur(5px) brightness(84%); */
-    
+
 }
+
 .v-card-title {
     margin-top: 30px;
     margin-left: 30px;
     margin-bottom: 30px;
 }
+
 /* .login_btn{
     margin-right: 30px;
 } */
