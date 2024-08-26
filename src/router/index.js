8ebfb9d61c435d8d5791c9a900f72158c8fdca37
Zhu-Peter <gzhu@ualberta.ca>
2024-08-25 22:35:59 -0600

    added /profile view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import SignupView from '@/views/SignupView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/signup',
     name: 'Sign Up',
     component: SignupView
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfileView
   },
   {
     path: '/about',
