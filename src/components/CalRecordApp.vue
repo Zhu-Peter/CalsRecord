37c797cefb84e8dc58a57eabff4c74ce824e12eb
Zhu-Peter <gzhu@ualberta.ca>
2024-08-26 17:09:03 -0600

    changed v-card to dropdown expansion panel vuetify object

--- src/components/CalRecordApp.vue
+++ src/components/CalRecordApp.vue
@@ -4,9 +4,10 @@
             <v-row>
                 <v-col>
                     <v-card width="100%" height="90%" class="elevation-1 my-8 pa-8">
-                        <v-card-title-text>
-                            <span class="headline">Enter a food</span>
-                        </v-card-title-text>
+                        <v-card-title class="mb-6">
+                            <span class="text-h3">Enter a food for <span class="text-secondary">{{ currentSelection
+                                    }}</span></span>
+                        </v-card-title>
                         <!-- search bar -->
                         <v-text-field label="Search" v-model="search" prepend-icon="mdi-magnify"></v-text-field>
 
@@ -16,38 +17,69 @@
             </v-row>
             <v-row>
                 <v-col>
-                    <v-card class="breakfast">
-                        <v-row>
-                            <v-col></v-col>
-                            <v-col>
-                                <v-btn color="primary" @click="addBreakfast">Add Breakfast</v-btn>
-                            </v-col>
-                        </v-row>
-                    </v-card>
-                    <v-card class="lunch">
-                        <v-row>
-                            <v-col></v-col>
-                            <v-col>
-                                 <v-btn color="primary" @click="addLunch">Add Lunch</v-btn>
-                             </v-col>
-                        </v-row>
-                    </v-card>
-                    <v-card class="dinner">
-                        <v-row>
-                            <v-col></v-col>
-                            <v-col>
-                                <v-btn color="primary" @click="addDinner">Add Dinner</v-btn>
-                            </v-col>
-                        </v-row>
-                    </v-card>
-                    <v-card class="snacks">
-                        <v-row>
-                            <v-col></v-col>
-                            <v-col justify="end">
-                                 <v-btn color="primary" @click="addSnacks">Add Snacks</v-btn>
-                             </v-col>
-                        </v-row>
-                    </v-card>
+                    <v-expansion-panels variant="accordion">
+                        <v-expansion-panel class="breakfast">
+                            <v-expansion-panel-title>
+                                <v-row>
+                                    <v-col cols="7" sm="10" md="11">
+                                        <div class="text-h5">Breakfast</div>
+                                    </v-col>
+                                    <v-col justify-self="end" cols="1">
+                                        <v-btn flat color="primary">Add</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-expansion-panel-title>
+                            <v-expansion-panel-text>
+                                <v-row v-for="food in breakfast" :key="food.name">
+                                    <v-col cols="4" sm="5">
+                                        <div class="text-h5">{{ food.name }}</div>
+                                    </v-col>
+                                    <v-col cols="4" sm="5">
+                                        <div class="text-h5">{{ food.cals }}</div>
+                                    </v-col>
+                                    <v-col justify-self="end" cols="1">
+                                        <v-btn flat color="primary">Remove</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-expansion-panel-text>
+                        </v-expansion-panel>
+                        <v-expansion-panel class="lunch">
+                            <v-expansion-panel-title>
+                                <v-row>
+                                    <v-col cols="7" sm="10" md="11">
+                                        <div class="text-h5">Lunch</div>
+                                    </v-col>
+                                    <v-col justify-self="end" cols="1">
+                                        <v-btn flat color="primary">Add</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-expansion-panel-title>
+                        </v-expansion-panel>
+                        <v-expansion-panel class="dinner">
+                            <v-expansion-panel-title>
+                                <v-row>
+                                    <v-col cols="7" sm="10" md="11">
+                                        <div class="text-h5">Dinner</div>
+                                    </v-col>
+                                    <v-col justify-self="end" cols="1">
+                                        <v-btn flat color="primary">Add</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-expansion-panel-title>
+                        </v-expansion-panel>
+                        <v-expansion-panel class="snacks">
+                            <v-expansion-panel-title>
+                                <v-row>
+                                    <v-col cols="7" sm="10" md="11">
+                                        <div class="text-h5">Snacks/Other</div>
+                                    </v-col>
+                                    <v-col justify-self="end" cols="1">
+                                        <v-btn flat color="primary">Add</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-expansion-panel-title>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
                 </v-col>
             </v-row>
         </v-container>
@@ -59,36 +91,40 @@ export default {
     name: "CalRecordApp",
     data() {
         return {
-            search: ""
+            search: "",
+            currentSelection: "Breakfast",
+            breakfast: [], lunch: [], dinner: [], snacks: []
         }
     }
 }
 </script>
 
 <style scoped>
-.breakfast{
+.breakfast {
     padding: 10px;
     margin: 10px;
     border-radius: 10px;
     border: 3px solid rgb(255, 255, 172);
 }
-.lunch{
+
+.lunch {
     padding: 10px;
     margin: 10px;
     border-radius: 10px;
     border: 3px solid rgb(255, 203, 172);
 }
-.dinner{
+
+.dinner {
     padding: 10px;
     margin: 10px;
     border-radius: 10px;
     border: 3px solid rgb(172, 194, 255);
 }
-.snacks{
+
+.snacks {
     padding: 10px;
     margin: 10px;
     border-radius: 10px;
     border: 3px solid rgb(172, 255, 200);
 }
-
 </style>
